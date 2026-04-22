a3f5fceb2d190dde9e63bdc6cdf1bca577421730
Md Shabbir Alam <mdshabbiralam007@gmail.com>
2026-04-22 14:16:27 +0530

    - project code added.

---
+++ CustomBrowser/CustomBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBrowserApp.swift
+//  CustomBrowser
+//
+//  Created by Md Shabbir Alam on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ CustomBrowserTests/CustomBrowserTests.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBrowserTests.swift
+//  CustomBrowserTests
+//
+//  Created by Md Shabbir Alam on 22/04/26.
+//
+
+import Testing
+
+struct CustomBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
